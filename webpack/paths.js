085190e2f84c8530f1ad26dc5370266a7832e84e
Dawid Nowakowski <dn@ges.sh>
2019-07-25 15:56:21 +0200

    Add empty folders to project structure and move webpack path

--- webpack/paths.js
+++ webpack/paths.js
@@ -3,4 +3,5 @@ const path = require('path');
 module.exports = {
   outputPath: path.resolve(__dirname, '../', 'dist'),
   entryPath: path.resolve(__dirname, '../', 'web', 'index.js'),
+  templatePath: path.resolve(__dirname, 'web', 'index.html')
 };
