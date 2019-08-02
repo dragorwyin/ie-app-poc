84c670899cddb39c0eb29fcc90e9fe6d891f59eb
Dawid Nowakowski <dn@ges.sh>
2019-08-02 11:11:50 +0200

    Add build commands for mobile

--- App.js
+++ App.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Clicker from './components/Clicker/Clicker';
 
-import rootReducers from "./redux/initStore";
+import rootReducers from "./redux/rootReducers";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default function App() {
   const store = createStore(rootReducers);
@@ -18,11 +27,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
