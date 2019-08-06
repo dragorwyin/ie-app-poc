d3b4a16eb4db3f7d746e819c167308ed5c2fb38d
Dawid Nowakowski <dn@ges.sh>
2019-08-06 13:48:28 +0200

    Add Storybook

--- web/components/Header/Header.jsx
+++ web/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Header as HeaderStyled, NavLink } from './HeaderStyled';
 
-const Header = () => {
+const Header = ({ active = 0 }) => {
   return (
     <HeaderStyled>
       <nav>
-        <NavLink href="/campaigns" active>Campaigns</NavLink>
-        <NavLink href="/templates">Templates</NavLink>
-        <NavLink href="/lists">Lists</NavLink>
+        <NavLink href="/campaigns" active={active === 0}>{name}</NavLink>
+        <NavLink href="/templates" active={active === 1}>Templates</NavLink>
+        <NavLink href="/lists" active={active === 2}>Lists</NavLink>
       </nav>
     </HeaderStyled>
   );
